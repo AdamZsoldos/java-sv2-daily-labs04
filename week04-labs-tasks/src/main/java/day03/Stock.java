7925578862e4c9e647dd150d54dac4cbd7b7f2f7
AdamZsoldos <adam.zsoldos@reddwarfgames.com>
2021-11-04 20:18:51 +0100

    Updated day03.Stock and StockMain

--- week04-labs-tasks/src/main/java/day03/Stock.java
+++ week04-labs-tasks/src/main/java/day03/Stock.java
@@ -1,5 +1,6 @@
 package day03;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Stock {
@@ -19,6 +20,20 @@ public class Stock {
             for (int j = i + 1; j < stocks.size(); j++) {
                 if (stocks.get(j) > max) max = stocks.get(j);
             }
+            if (max - min > result) result = max - min;
+        }
+        return result;
+    }
+
+    public double maxProfitWithSubList() {
+        double result = 0;
+        List<Double> subList;
+        double min;
+        double max;
+        for (int i = 0; i < stocks.size() - 1; i++) {
+            subList = stocks.subList(i + 1, stocks.size());
+            min = stocks.get(i);
+            max = Collections.max(subList);
             if (max - min > result) result = max - min;
         }
         return result;
